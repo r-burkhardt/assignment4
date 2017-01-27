5ee6831610fbefeaafb0d9ba6da09eca3ab1a8f5
rburkhardt <rburkhardt@rodericks-mbp.hsd1.ca.comcast.net>
2017-01-26 21:30:20 -0800

    continuing work on DataMatrix

--- Assignment4/src/assignment4/Assignment4.java
+++ Assignment4/src/assignment4/Assignment4.java
@@ -54,17 +54,64 @@ class BarcodeImage implements Cloneable // Faye
 
 class DataMatrix implements BarcodeIO // Roderick
 {
+    public static final char BLACK_CHAR = '*';
+    public static final char WHITE_CHAR = ' ';
+    private BarcodeImage image;
+    private String text;
+    private int actualWidth;
+    private int actualHeight;
+    
     public DataMatrix()
     {
-        
+        image = new BarcodeImage();
+        text = " ";
+        actualWidth = 0;
+        actualHeight = 0;
     }
     
     public DataMatrix(BarcodeImage image)
     {
-        
+        image = scan(image);
+        text = " ";
     }
     
     public DataMatrix(String text)
+    {
+        image = new BarcodeImage();
+        text = readText(text);
+    }
+    
+    public boolean readText(String text)
+    {
+        return true;
+    }
+    
+    public boolean scan(BarcodeImage image)
+    {
+        return true;
+    }
+    
+    public int getActualWidth()
+    {
+        return actualWidth;
+    }
+    
+    public int getActualHeight()
+    {
+        return actualHeight;
+    }
+    
+    private int computeSignalWidth()
+    {
+        return 0;
+    }
+    
+    private int computeSignalHeight()
+    {
+        return 0;
+    }
+    
+    private void cleanImage()
     {
         
     }
